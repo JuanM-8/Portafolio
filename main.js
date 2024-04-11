a2e71969f1da951dec257e0099321b5dda9a4b1d
juan0088 <juandavidmarin.0008@gmail.com>
2024-04-11 15:29:21 -0500

    fix nav

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const aboutA = document.querySelectorAll("#aboutA");
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
+})
+aboutA.forEach(element => {
+    element.addEventListener("click", () => {
+        nav.classList.remove("visible");
+    })
 })
 
 cerrar.addEventListener("click", () => {
