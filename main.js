25181701fdebe7ce564f55bf876aab90773f6cbf
juan0088 <juandavidmarin.0008@gmail.com>
2024-04-13 16:34:52 -0500

    Separador & animaciones

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 const aboutA = document.querySelectorAll("#aboutA");
+const separador = document.getElementById("separador");
+const sssvg = document.getElementById("ssvg");
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
@@ -15,3 +17,16 @@ aboutA.forEach(element => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY === 0) {
+        // El usuario ha vuelto al principio de la página, muestra el separador
+        
+        sssvg.style.stroke = "#ffffff";
+    } else {
+        // El usuario no está en la parte superior de la página, oculta el separador
+        sssvg.style.stroke = "transparent";
+    }
+});
+
+AOS.init();
